3a0ce46f0e0960712c9c40edc8ca16f906545416
shulu <961085397@qq.com>
2024-09-18 17:49:10 +0800

    chapter 5 mybatis-plus

--- restful/src/main/java/com/restful/entity/Book.java
+++ restful/src/main/java/com/restful/entity/Book.java
@@ -1,5 +1,7 @@
 package com.restful.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@TableName("book")
 public class Book {
 
     private int id;
