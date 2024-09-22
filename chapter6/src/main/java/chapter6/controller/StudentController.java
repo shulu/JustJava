87d7c059132717550157e88afe4ae5d5964ba07c
shulu <shulu@users.noreply.github.com>
2024-09-22 23:26:43 +0800

    finish chapter 6

--- chapter6/src/main/java/chapter6/controller/StudentController.java
+++ chapter6/src/main/java/chapter6/controller/StudentController.java
@@ -1,20 +1,22 @@
 package chapter6.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
 import chapter6.entity.Result;
 import chapter6.entity.Student;
 import chapter6.service.IStudentService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/students")
@@ -24,25 +26,37 @@ public class StudentController {
     IStudentService iStudentService;
 
     @GetMapping
-    public Result<?> findAllStudents() {
-        List<Student> students = iStudentService.list();
-        return Result.success(students);
+    public Result<?> findPage(@RequestParam(name = "pageNum", defaultValue = "1") Integer pageNum,
+            @RequestParam(name = "pageSize", defaultValue = "5") Integer pageSize,
+            @RequestParam(name = "search", defaultValue = "", required = false) String search) {
+        Page<Student> page = iStudentService.page(new Page<>(pageNum, pageSize),
+                Wrappers.<Student>lambdaQuery().like(Student::getStudentname, search));
+        return Result.success(page);
     }
+
+    // @GetMapping
+    // public Result<?> findAllStudents() {
+    // List<Student> students = iStudentService.list();
+    // return Result.success(students);
+    // }
 
     @PostMapping
     public Result<?> save(@RequestBody Student student) {
+        System.out.println(student);
         iStudentService.save(student);
         return Result.success();
     }
 
     @PutMapping
     public Result<?> update(@RequestBody Student student) {
+        System.out.println(student);
         iStudentService.updateById(student);
         return Result.success();
     }
 
-    @DeleteMapping
-    public Result<?> delete(@PathVariable long id) {
+    @DeleteMapping("/{id}")
+    public Result<?> delete(@PathVariable("id") long id) {
+        System.out.println(id);
         iStudentService.removeById(id);
         return Result.success();
     }
--- chapter6/src/main/java/chapter6/mapper/StudentMapper.java
+++ chapter6/src/main/java/chapter6/mapper/StudentMapper.java
@@ -1,12 +1,9 @@
 package chapter6.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import chapter6.entity.Student;
 
-@Mapper
 public interface StudentMapper extends BaseMapper<Student> {
 
 }
